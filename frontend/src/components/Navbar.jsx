60c0afab1f65feecad39c5a24cd3d29f95de1a15
adarsh-k2605 <addyraj145@gmail.com>
2026-02-12 00:18:51 +0530

    fixed typos

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,6 @@ import { UserButton } from "@clerk/clerk-react";
 function Navbar() {
   const location = useLocation();
 
-  console.log(location);
 
   const isActive = (path) => location.pathname === path;
 
@@ -62,7 +61,7 @@ function Navbar() {
           >
             <div className="flex items-center gap-x-2.5">
               <LayoutDashboardIcon className="size-4" />
-              <span className="font-medium hidden sm:inline">Dashbord</span>
+              <span className="font-medium hidden sm:inline">Dashboard</span>
             </div>
           </Link>
 
